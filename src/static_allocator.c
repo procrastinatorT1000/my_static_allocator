efb7ba09bf1e277a2029449afd7b46cf919efe9d
Slobodchikov Roman <romka-sl@vtomske.ru>
2018-02-13 23:07:43 +0700

    Добавлена функция для выделения памяти

--- src/static_allocator.c
+++ src/static_allocator.c
@@ -18,12 +18,13 @@ typedef struct
 
 static DataSegmentUsage_t dataSegm[SEGMENT_NUM] = {0};
 static char isInitialised = FALSE;
+static size_t segmByteSize = 0;
 
 int initStaticAllocator(char *data, size_t byteLen)
 {
-	size_t segByteSize = byteLen / SEGMENT_NUM;
+	size_t segBSize = byteLen / SEGMENT_NUM;
 
-	if(segByteSize < SEGMENT_MIN_BSIZE)
+	if(segBSize < SEGMENT_MIN_BSIZE)
 	{
 		perror("Error: Static memory byte len should be bigger or equal to %d bytes\n", SEGMENT_NUM * SEGMENT_MIN_BSIZE);
 
@@ -41,13 +42,50 @@ int initStaticAllocator(char *data, size_t byteLen)
 
 	for(int i = 0; i < SEGMENT_NUM; i++)
 	{
-		dataSegm.dataPtr = &data[i*segByteSize];
-		dataSegm.isUsed = FALSE;
+		dataSegm[i].dataPtr = &data[i*segBSize];
+		dataSegm[i].isUsed = FALSE;
 	}
 	isInitialised = TRUE;
+	segmByteSize = segBSize;
 
 	return TRUE;
 }
 
-//char * malloc()
+char * my_malloc(size_t bytenum)
+{
+	if(isInitialised == FALSE)
+	{
+		perror("Static allocator isn't initialized\n");
+		return NULL;
+	}
+
+	int segNum = bytenum / segmByteSize + 1;
+	int counter = 0;
+	int allocSegIdx = 0;
+	char *allocPtr = NULL;
+
+	int idx = 0;
+	for(idx = 0; idx < SEGMENT_NUM; idx++)
+	{
+		if(dataSegm[idx].isUsed == FALSE)
+			counter++;
+		if(counter >= segNum)
+			break;
+	}
+
+	if(idx == SEGMENT_NUM)
+	{
+		return NULL;	/* No free mem */
+	}
+
+	int startAllocIdx = idx - segNum + 1;
+	allocPtr = dataSegm[startAllocIdx].dataPtr;
+
+	for(int k = startAllocIdx; k <= idx; k++ )
+	{
+		dataSegm[k].isUsed = TRUE;
+	}
+
+	return allocPtr;
+}
 
